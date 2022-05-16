1242c122106dc749cea98f621bd95de85c54d383
alexisoleinikoff <alexis.oleinikoff@heig-vd.ch>
2022-05-17 00:20:03 +0200

    Update wifi_co2.py

--- wifi_co2.py
+++ wifi_co2.py
@@ -1,59 +1,19 @@
-# import module
-import os
+import subprocess
 
-# function to establish a new connection
-def createNewConnection(name, SSID, password):
-	config = """<?xml version=\"1.0\"?>
-<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-	<name>"""+name+"""</name>
-	<SSIDConfig>
-		<SSID>
-			<name>"""+SSID+"""</name>
-		</SSID>
-	</SSIDConfig>
-	<connectionType>ESS</connectionType>
-	<connectionMode>auto</connectionMode>
-	<MSM>
-		<security>
-			<authEncryption>
-				<authentication>WPA2PSK</authentication>
-				<encryption>AES</encryption>
-				<useOneX>false</useOneX>
-			</authEncryption>
-			<sharedKey>
-				<keyType>passPhrase</keyType>
-				<protected>false</protected>
-				<keyMaterial>"""+password+"""</keyMaterial>
-			</sharedKey>
-		</security>
-	</MSM>
-</WLANProfile>"""
-	command = "netsh wlan add profile filename=\""+name+".xml\""+" interface=Wi-Fi"
-	with open(name+".xml", 'w') as file:
-		file.write(config)
-	os.system(command)
+ssid = "HHJ-43488"
+passkey = "42xi-4d8v-qany-ixxu"
 
-# function to connect to a network
-def connect(name, SSID):
-	command = "netsh wlan connect name=\""+name+"\" ssid=\""+SSID+"\" interface=Wi-Fi"
-	os.system(command)
+p1 = subprocess.Popen(
+    ["wpa_passphrase", ssid, passkey], 
+    stdout=subprocess.PIPE
+)
 
-# function to display avavilabe Wifi networks
-def displayAvailableNetworks():
-	command = "netsh wlan show networks interface=Wi-Fi"
-	os.system(command)
+p2 = subprocess.Popen(
+    ["sudo", "tee", "a", "/etc/wpa_supplicant/wpa_supplicant.conf", ">", "/dev/null"], 
+    stdin=p1.stdout, 
+    stdout=subprocess.PIPE
+)
 
+p1.stdout.close()  # Give p1 a SIGPIPE if p2 dies.
 
-# display available netwroks
-displayAvailableNetworks()
-
-# input wifi name and password
-name = input("Name of Wi-Fi: ")
-password = input("Password: ")
-
-# establish new connection
-createNewConnection(name, name, password)
-
-# connect to the wifi network
-connect(name, name)
-print("If you aren't connected to this network, try connecting with the correct password!")
+output,err = p2.communicate()
