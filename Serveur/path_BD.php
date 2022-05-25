5d02ca36dc8fb8de6ce15e430bd85095be7e7c44
alexisoleinikoff <alexis.oleinikoff@heig-vd.ch>
2022-05-25 18:21:09 +0200

    Update path_BD.php

--- Serveur/path_BD.php
+++ Serveur/path_BD.php
@@ -1,5 +1,5 @@
 <?php
-	$mon_Path_BD ='mysql:host=localhost:3306;dbname=trackswim;charset=utf8';
+	$mon_Path_BD ='mysql:host=localhost:3306;dbname=trackswim_db;charset=utf8';
 	$mon_userName ='root';
 	$monPSW = '';
 ?>
