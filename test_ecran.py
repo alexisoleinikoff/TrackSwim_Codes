3c448031938874557a44174cc5fac8abd6ce6967
alexis <alexis.oleinikoff@gmail.com>
2022-06-28 17:40:20 +0200

    modifié :         test_ecran.py 	supprimé :        tm1637_old/tm1637_6digit.py 	supprimé :        tm1637_old/tm1637_autre_fichier.py

--- test_ecran.py
+++ test_ecran.py
@@ -11,25 +11,23 @@ GPIO.setwarnings(False)
 # Constantes
 LED_JAUNE = 4
 LED_BLEU = 27
-BOUTTON = 17
+BOUTON = 17
 CLK_VERT = 2
 DIO_VERT = 3
 CLK_BLEU = 23
 DIO_BLEU = 24
-T_UPDATE_SCREEN = 20
+T_UPDATE_SCREEN = 20 #ms
+ANTI_REBOND = 50 #ms
 
 # Initialisation des GPIO
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(LED_JAUNE, GPIO.OUT)
 GPIO.setup(LED_BLEU, GPIO.OUT)
-GPIO.setup(BOUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BOUTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.output(LED_BLEU, GPIO.LOW)
 GPIO.output(LED_JAUNE, GPIO.LOW)
-
-t1 = millis()
-t_initial = t1
 
 # Initialisation des écrans
 tmd = tm1637.TM1637(clk=CLK_VERT, dio=DIO_VERT) #GPIO NUM, écran vert, en haut
@@ -41,9 +39,28 @@ tmd.brightness(7)
 tmt.write([0,0,0,0,0,0]) # valeurs initiales (écran vide)
 tmd.write([0,0,0,0,0,0])
 
+# Variables
+t1 = millis()
+t_initial = t1
+state_timer = t1
+state = False
+
 # Main
 try:
     while True:
+        # Gestion de l'input
+        if not GPIO.input(BOUTON):
+            state_timer = millis()
+            state = True
+        elif millis() - state_timer > ANTI_REBOND:
+            state = False
+        else:
+            pass
+
+        if(state):
+            t_initial = millis()
+
+        # Mise à jour de l'écran
         if millis() - t1 > T_UPDATE_SCREEN:
             t1 = millis()
             tmd.write(tmd.encode_string(millis_to_mmssms(t_initial, millis())))
